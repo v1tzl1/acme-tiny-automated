fffc36bd670999b62019861827b9e5c79352042c
Martin Luelf <mail@mluelf.de>
2017-05-28 20:53:56 +0200

    fix path

--- bin/run-acme-tiny.sh
+++ bin/run-acme-tiny.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ../config.sh
+source `dirname $0`/../config.sh
 
 if [[ $USER != ${USER_ACME} ]]; then
     echo "This script is only supposed to be run by the user ${USER_ACME}";
