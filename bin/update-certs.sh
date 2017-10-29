38d338bb6b5a1564ab32b3ebfdb66fa2d30537d5
Martin Luelf <mail@mluelf.de>
2017-10-29 12:09:27 +0100

    Add automatic update routine (echo only so far)

--- bin/update-certs.sh
+++ bin/update-certs.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-echo "Not implemented yet"
+source `dirname $0`/../config.sh
+
+if [[ $USER != ${USER_CRON} ]]; then
+    echo "This script is only supposed to be run by the user ${USER_CRON}";
+    exit 1;
+fi
+
+cd "${DIR_CERTS}/configs"
+for config in *.cnf
+do
+	DOMAIN="${config%.cnf}"
+
+	# Is domain still valid in the next 20 days
+	openssl x509 -checkend $((20*24*3600)) -noout -in "../live/${DOMAIN}.crt";
+	EXPIRE_SOON="$?"
+
+	if [ ${EXPIRE_SOON} -eq 1 ]; then
+		echo "Renewing ${DOMAIN}"
+		echo "ECHO: sudo -u ${USER_GEN} ${DIR_CERTS}/bin/new_cert.sh ${DOMAIN}"
+	else
+		echo "Domain ${DOMAIN} is still valid"
+	fi
+
+done
